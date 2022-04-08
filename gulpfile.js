f80c9ce8eb84bdc407ad348f8b1ef7a3f6026010
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2022-04-08 15:29:47 +0200

    Added gulp-task ´test´

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const typescript = require('gulp-typescript');
 
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
 const tsProject = typescript.createProject("tsconfig.json");
 
 const scssFiles = 'src/scss/**/*.scss';
@@ -20,4 +23,21 @@ gulp.task('ts', function (ac) {
 	return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest(jsDest));
 })
 
-exports.build = gulp.parallel('scss', 'ts');
+gulp.task('build', gulp.parallel('scss', 'ts'));
+
+gulp.task('reload', function (ac) {
+	reload({ stream: true });
+	ac();
+});
+
+gulp.task('test', gulp.series('scss', 'ts', function (ac) {
+	browserSync({
+		server: {
+			baseDir: 'test'
+		}
+	});
+
+	gulp.watch('src/scss/**/*.scss', gulp.series('scss', 'reload'));
+	gulp.watch('src/ts/**/*.ts', gulp.series('ts', 'reload'));
+	gulp.watch('test/**/*.html', gulp.series('reload'));
+}));
