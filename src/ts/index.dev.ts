cb7c8b714653e44e7804dacd3f9b3a22674d1976
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2022-05-07 10:52:43 +0200

    mapper, test | Added test for the `StepByStepMapper`

--- src/ts/index.dev.ts
+++ src/ts/index.dev.ts
@@ -1,8 +1,11 @@
 import Sintro from './sintroduce';
 import { VERSION, AUTHOR, LICENSE } from './metadata';
 
+/* expose parts for testing */
 import Queue from './lib/queue';
 import Stack from './lib/stack';
+
+import { Instruction, SetPresent } from './lib/instruction';
 
 /* expose instance of
  * the Sintroduce class
@@ -13,4 +16,5 @@ Sintroduce.VERSION = VERSION + "-dev";
 Sintroduce.AUTHOR = AUTHOR;
 Sintroduce.LICENSE = LICENSE;
 
-export { Sintroduce, Sintro, Queue, Stack };
+/* export everything */
+export { Sintroduce, Sintro, Queue, Stack, Instruction, SetPresent };
